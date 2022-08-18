665b7b9cde8c97d960e2e73f8b04d31dd3c4dfea
mrLuisFer <mrluisfeer@gmail.com>
2022-08-17 21:34:55 -0500

    update styles in tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,6 +5,16 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      'body': ["'Karla'", "system-ui"]
+    },
+    colors: {
+      'brightyellow': 'hsl(71, 73%, 54%)',
+      'lightgray': 'hsl(204, 43%, 93%)',
+      'grayishblue': 'hsl(218, 22%, 67%)',
+      'varCyan': 'hsl(179, 62%, 43%)',
+      'white': '#ffffff'
+    },
     extend: {},
   },
   plugins: [],
